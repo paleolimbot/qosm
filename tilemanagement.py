0e6834f4006285d9c282c7adef83300168406b6d
Dewey Dunnington <dewey@fishandwhistle.net>
2016-01-04 15:08:56 -0500

    add/fix builtin tile types

--- tilemanagement.py
+++ tilemanagement.py
@@ -10,7 +10,32 @@ import random
 
 BUILT_IN_TILETYPES = {"osm":["http://a.tile.openstreetmap.org/${z}/${x}/${y}.png",
                              "http://b.tile.openstreetmap.org/${z}/${x}/${y}.png",
-                             "http://c.tile.openstreetmap.org/${z}/${x}/${y}.png"]}
+                             "http://c.tile.openstreetmap.org/${z}/${x}/${y}.png"],
+                      "stamenbw": ["http://a.tile.stamen.com/toner/${z}/${x}/${y}.png",
+                                   "http://b.tile.stamen.com/toner/${z}/${x}/${y}.png"
+                                   "http://c.tile.stamen.com/toner/${z}/${x}/${y}.png"],
+                      "thunderforestlandscape": ["http://a.tile.thunderforest.com/landscape/${z}/${x}/${y}.png",
+                                                "http://b.tile.thunderforest.com/landscape/${z}/${x}/${y}.png",
+                                                "http://c.tile.thunderforest.com/landscape/${z}/${x}/${y}.png"],
+                      "thunderforestoutdoors": ["http://a.tile.thunderforest.com/outdoors/${z}/${x}/${y}.png",
+                                                "http://b.tile.thunderforest.com/outdoors/${z}/${x}/${y}.png",
+                                                "http://c.tile.thunderforest.com/outdoors/${z}/${x}/${y}.png"],
+                      "hillshade":"http://c.tiles.wmflabs.org/hillshading/${z}/${x}/${y}.png",
+                      "stamenwatercolor":["http://a.tile.stamen.com/watercolor/${z}/${x}/${y}.jpg",
+                                          "http://b.tile.stamen.com/watercolor/${z}/${x}/${y}.jpg",
+                                          "http://c.tile.stamen.com/watercolor/${z}/${x}/${y}.jpg"],
+                      "mapquestsat":["http://otile1.mqcdn.com/tiles/1.0.0/sat/${z}/${x}/${y}.jpg",
+                                     "http://otile2.mqcdn.com/tiles/1.0.0/sat/${z}/${x}/${y}.jpg",
+                                     "http://otile3.mqcdn.com/tiles/1.0.0/sat/${z}/${x}/${y}.jpg",
+                                     "http://otile4.mqcdn.com/tiles/1.0.0/sat/${z}/${x}/${y}.jpg"]}
+
+BUILT_IN_MAXZOOM = {"osm":19,
+                  "stamenbw": 19,
+                  "thunderforestlandscape": 19,
+                  "thunderforestoutdoors": 19,
+                  "hillshade": 14, #can be less in some areas
+                  "stamenwatercolor":15,
+                  "mapquestsat":11} #only 8 in rosm?
 
 def tileurl(tiletype, tile, zoom, suffix=""):
     '''
@@ -19,7 +44,9 @@ def tileurl(tiletype, tile, zoom, suffix=""):
     urls for multiple tile servers?
     '''
     if tiletype in BUILT_IN_TILETYPES:
-        pattern = random.sample(BUILT_IN_TILETYPES[tiletype], 1)[0]
+        pattern = BUILT_IN_TILETYPES[tiletype]
+        if isinstance(pattern, list):
+            pattern = random.sample(pattern, 1)[0]
     else:
         pattern = tiletype
     return pattern.replace("$", "").format(z=zoom, x=tile[0], y=tile[1])+suffix
@@ -43,7 +70,10 @@ def auxfilename(fname):
     return fname + ".aux.xml"
 
 def maxzoom(tiletype):
-    return 21
+    if tiletype in BUILT_IN_MAXZOOM:
+        return BUILT_IN_MAXZOOM[tiletype]
+    else:
+        return 20
 
 def minzoom(tiletype):
     return 1
