3616376faf82ac85f57af6a6b135bf4b082887b3
Dewey Dunnington <dewey@fishandwhistle.net>
2016-01-04 09:32:40 -0500

    split worker functions and layer-modifying functions in attempt to not crash while rendering

--- qosmtilelayer.py
+++ qosmtilelayer.py
@@ -49,21 +49,20 @@ class QOSMTileLayer(QgsPluginLayer):
         self.loadedtiles.clear()
     
     def refreshtiles(self, canvasextent, canvascrs, widthpx, triggerrepaint=False):
+        tilestoclean, tilestoload, tilefiles = self.refreshtiles_get(canvasextent, canvascrs, widthpx)
+        self.refreshtiles_apply(tilestoclean, tilestoload, tilefiles)
+        if triggerrepaint:
+            self.triggerRepaint()
+    
+    def refreshtiles_get(self, canvasextent, canvascrs, widthpx):
         xform = QgsCoordinateTransform(canvascrs,
                                     QgsCoordinateReferenceSystem(4326))
         extll = xform.transform(canvasextent)
         zoom = osm.autozoom(widthpx/(extll.xMaximum()-extll.xMinimum()))
         tiles = osm.tiles(extll.xMinimum(), extll.xMaximum(), 
                           extll.yMinimum(), extll.yMaximum(), zoom)
-        
-        reg = QgsMapLayerRegistry.instance()
-        
+
         tilestoclean = self.loadedtiles.difference(set(tiles))
-        for tile in tilestoclean:
-            reg.removeMapLayer(self.loadedlayers[tile])
-            del self.loadedlayers[tile]
-            self.loadedtiles.remove(tile)
-        
         tilestoload = list(set(tiles).difference(self.loadedtiles))
         
         #calculate file names and urls
@@ -72,8 +71,18 @@ class QOSMTileLayer(QgsPluginLayer):
         
         #download (keep on same thread for now)
         downloader.download(tileurls, tilefiles)
+        return tilestoclean, tilestoload, tilefiles
+    
+    def refreshtiles_apply(self, tilestoclean, tilestoload, tilefiles):
+        reg = QgsMapLayerRegistry.instance()
+        #clean
+        for tile in tilestoclean:
+            reg.removeMapLayer(self.loadedlayers[tile])
+            del self.loadedlayers[tile]
+            self.loadedtiles.remove(tile)
         
-        #go through by tile and write aux information and load if necessary
+        #load
+        extent = None
         for i in range(len(tilestoload)):
             #check file exists
             if os.path.exists(tilefiles[i]):
@@ -87,6 +96,10 @@ class QOSMTileLayer(QgsPluginLayer):
                     layer = reg.addMapLayer(layer, False)
                     self.loadedlayers[tilestoload[i]] = layer.id()
                     self.loadedtiles.add(tilestoload[i])
+                    if extent is None:
+                        extent = layer.extent()
+                    else:
+                        extent = extent.combineExtentWith(layer.extent())
                 else:
                     #report error?
                     pass
@@ -94,38 +107,42 @@ class QOSMTileLayer(QgsPluginLayer):
             else:
                 #report error?
                 pass
+        if not extent is None:    
+            self.setExtent(extent)
+    
+    def createimage(self, extent, crs, outputsize):
+        render = QgsMapRenderer()
+        render.setLayerSet(self.loadedlayers.values())
+        render.setProjectionsEnabled(True)
+        render.setDestinationCrs(crs)
+        render.setExtent(extent)
         
-        self.setExtent(canvasextent)
-        if triggerrepaint:
-            self.triggerRepaint()
+        img = QImage(outputsize, QImage.Format_ARGB32_Premultiplied)
+        img.fill(Qt.transparent) #needed, apparently the QImage() is not empty
+                    
+        painter = QPainter()
+        painter.begin(img)
+        painter.setRenderHint(QPainter.Antialiasing)
+        render.setOutputSize(img.size(), img.logicalDpiX())
+        render.render(painter)
+        painter.end()
+        return img
     
     def draw(self, rendererContext):
         #should be done on a different thread: invoke multithreaded rendering?
-        self.refreshtiles(rendererContext.extent(), 
-                   rendererContext.coordinateTransform().destCRS(),
-                   rendererContext.painter().device().width(), triggerrepaint=False)
+        extent = rendererContext.extent()
+        crs = rendererContext.coordinateTransform().destCRS()
+        outputsize = QSize(rendererContext.painter().device().width(),
+                        rendererContext.painter().device().height())
         
+        tilestoclean, tilestoload, tilefiles = self.refreshtiles_get(extent, crs, outputsize.width())
+        #check if rendering stopped before applying changes to object
+        if rendererContext.renderingStopped():
+            return True
+        self.refreshtiles_apply(tilestoclean, tilestoload, tilefiles)
         
         if len(self.loadedlayers) > 0:
-             
-            render = QgsMapRenderer()
-            render.setLayerSet(self.loadedlayers.values())
-            render.setProjectionsEnabled(True)
-            render.setDestinationCrs(rendererContext.coordinateTransform().destCRS())
-            render.setExtent(rendererContext.extent())
-            
-            img = QImage(QSize(rendererContext.painter().device().width(),
-                        rendererContext.painter().device().height()),
-                  QImage.Format_ARGB32_Premultiplied)
-            img.fill(Qt.transparent) #needed, apparently the QImage() is not empty
-                        
-            painter = QPainter()
-            painter.begin(img)
-            painter.setRenderHint(QPainter.Antialiasing)
-            render.setOutputSize(img.size(), img.logicalDpiX())
-            render.render(painter)
-            painter.end()
-             
+            img = self.createimage(extent, crs, outputsize) 
             rendererContext.painter().drawImage(0, 0, img)            
         else:
             #no tiles. debug message?
